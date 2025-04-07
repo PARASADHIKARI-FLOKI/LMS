b3813abd5cbd29a0fea20ebab21b966c820fed3e
PARASADHIKARI-FLOKI <paras.adhikari@example.com>
2025-04-07 23:10:43 +0545

    your message here

--- src/components/student/Loading.jsx
+++ src/components/student/Loading.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Loading = () => {
   return (
-    <div>
-      
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="flex space-x-2">
+        {[...Array(3)].map((_, i) => (
+          <div
+            key={i}
+            className="w-3 h-3 bg-blue-500 rounded-full animate-bounce"
+            style={{ animationDelay: `${i * 0.2}s` }}
+          ></div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Loading
+export default Loading;
--- src/components/student/Navbar.jsx
+++ src/components/student/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   const {user}= useUser()
 
   return (
-    <div className={`flex justify-between items-center  px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'}`}>
+    <div className={`flex justify-between items-center px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'}`}>
       <img onClick={()=>navigate('/')} src={assets.logo} alt="logo"  className='w-28 lg:w-32 cursor-pointer'/>
       <div className='hidden md:flex items-center gap-5 text-gray-500'>
         <div className='flex items-center gap-5'>
